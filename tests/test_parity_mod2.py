ee65762b2d94ab5e0aa24bb9ca7db3c4bd4284ed
Sarina Barton <rememberalways1971@gmail.com>
2026-07-09 11:03:09 -0700

    Remove parity_mod2 rule from test file
    Removed the parity_mod2 rule and unnecessary comments.
    
    Signed-off-by: Sarina Barton <rememberalways1971@gmail.com>

--- tests/test_parity_mod2.py
+++ tests/test_parity_mod2.py
@@ -1,9 +1,5 @@
-# tests/parity_mod2.echo13
 define echo C1 = 1
 define echo C2 = 0
 define echo P1 = 1
 define echo P2 = 0
 define echo K  = 3
-
-rule parity_mod2
-    (C1 + C2) % 2 == (P1 + P2 + 2*K) % 2
